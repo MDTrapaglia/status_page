02232314f7b9a120d40b0b37d9f67e993c729a1d
Matias Trapaglia <matiasdanmansilla@gmail.com>
2026-05-06 17:00:00 -0300

    Add IBM stock symbol to markets tracking list

--- app.py
+++ app.py
@@ -95,6 +95,7 @@ STOCKS: List[Dict[str, str]] = [
     {"name": "Marvell Technology (MRVL)", "symbol": "MRVL", "tradingview": "NASDAQ:MRVL"},
     {"name": "Abbott Laboratories (ABT)", "symbol": "ABT", "tradingview": "NYSE:ABT"},
     {"name": "iShares Silver Trust (SLV)", "symbol": "SLV", "tradingview": "NYSEARCA:SLV"},
+    {"name": "International Business Machines (IBM)", "symbol": "IBM", "tradingview": "NYSE:IBM"},
     {"name": "USD/ARS", "symbol": "ARS=X", "tradingview": "FX_IDC:USDARS"},
 ]
 BINANCE_URL = "https://api.binance.com/api/v3/ticker/24hr"
